ea87de8d8f17716821cafd2c058b9f2a1fae0fb1
sailyw <yiwen.lu@bigita.com>
2021-06-03 10:28:12 +0800

    添加elementui

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,17 +3,55 @@
     <div class="div1">div1</div>
     <div class="div2">div2</div>
     <div class="div3">div3</div>
+    <el-row :gutter="20">
+      <el-col :span="4"
+        ><el-input v-model="input" placeholder="请输入内容"></el-input
+      ></el-col>
+      <el-col :span="8"
+        ><el-select v-model="value" placeholder="请选择">
+          <el-option
+            v-for="item in options"
+            :key="item.value"
+            :label="item.label"
+            :value="item.value"
+          >
+          </el-option> </el-select
+      ></el-col>
+      <el-col :span="8"></el-col>
+    </el-row>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
 export default {
   name: "Home",
-  components: {
-    HelloWorld,
+  data() {
+    return {
+      input: "",
+      options: [
+        {
+          value: "选项1",
+          label: "黄金糕",
+        },
+        {
+          value: "选项2",
+          label: "双皮奶",
+        },
+        {
+          value: "选项3",
+          label: "蚵仔煎",
+        },
+        {
+          value: "选项4",
+          label: "龙须面",
+        },
+        {
+          value: "选项5",
+          label: "北京烤鸭",
+        },
+      ],
+      value: "",
+    };
   },
 };
 </script>
